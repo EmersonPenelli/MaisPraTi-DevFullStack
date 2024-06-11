3d706513c2bfe3d169241b915f34008e44efc5fc
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-10 22:53:19 -0300

    Initial commit

---
+++ Modulo-01/Lista-de-Exercicios_2/Exercicio_24.js
@@ -0,0 +1,49 @@
+/*
+24. Dada uma matriz M[1..6,1..8], criar um vetor C que contenha, em cada posição, a
+quantidade de elementos negativos da linha correspondente de M.
+*/
+
+function imprimirMatriz(matriz) {
+  for (let i = 0; i < matriz.length; i++) {
+    console.log(matriz[i].join("	"));
+  }
+}
+
+function criarMatriz(linhas, colunas) {
+  const matriz = [];
+  for (let i = 0; i < linhas; i++) {
+    matriz[i] = [];
+    for (let j = 0; j < colunas; j++) {
+      matriz[i][j] = numeroAleatorio();
+    }
+  }
+  return matriz;
+}
+
+function criarVetorContadorNegativos(matriz) {
+  const VETOR = [];
+  for (let i = 0; i < matriz.length; i++) {
+    VETOR[i] = 0;
+    for (let j = 0; j < matriz[i].length; j++) {
+      if (matriz[i][j] < 0) {
+        VETOR[i]++;
+      }
+    }
+  }
+  return VETOR;
+}
+
+function numeroAleatorio() {
+  return Math.floor(Math.random() * 21) - 10; // Gera números entre -10 e 10
+}
+
+const LINHAS = 6;
+const COLUNAS = 8;
+
+const MATRIZ = criarMatriz(LINHAS, COLUNAS);
+
+const VETOR = criarVetorContadorNegativos(MATRIZ);
+
+imprimirMatriz(MATRIZ);
+
+console.log(VETOR);
