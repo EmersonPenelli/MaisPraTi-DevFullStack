4c1643601b09188d406642f40392ef5df655a211
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-11 23:06:56 -0300

    Initial commit

---
+++ Modulo-01/Lista-de-Exercicios_2/Exercicio_44.js
@@ -0,0 +1,25 @@
+/*
+44. Escreva uma função que conte quantas propriedades do tipo string existem em um
+objeto e retorne esse número.
+*/
+
+function contarPropriedadesString(objeto) {
+  let contador = 0;
+  for (let chave in objeto) {
+    if (typeof objeto[chave] === "string") {
+      contador++;
+    }
+  }
+  return contador;
+}
+
+const dados = {
+  prop1: [1, 2, 3],
+  prop2: "string2",
+  prop3: 42,
+  prop4: ["a", "b", "c"],
+  prop5: [true, false],
+  prop6: 123,
+};
+
+console.log(`${contarPropriedadesString(dados)}`);
