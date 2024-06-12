506b1b60481a560f73f72b6530892398d9aa5938
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-11 22:55:57 -0300

    Initial commit

---
+++ Modulo-01/Lista-de-Exercicios_2/Exercicio_26.js
@@ -0,0 +1,102 @@
+/*
+26. Dadas duas matrizes numéricas A[1..3,1..5] e B[1..3,1..5], calcular a matriz produto
+P[1..3,1..5].
+*/
+
+function imprimirMatriz(matriz) {
+  for (let i = 0; i < matriz.length; i++) {
+    console.log(matriz[i].join("	"));
+  }
+}
+
+function receberEntradaInteira(mensagem, limite) {
+  if (limite == undefined) {
+    limite = Number.MAX_SAFE_INTEGER;
+  }
+  let inteiro = "a";
+  do {
+    try {
+      inteiro = parseInt(prompt(mensagem));
+    } catch (error) {
+      console.log(
+        "Informe um número inteiro, positivo válido! (somente a parte inteira será considerada)"
+      );
+      inteiro = "a";
+    }
+    if (inteiro > limite) {
+      console.log(
+        "Informe um número inteiro, positivo válido menor ou igual a " +
+          limite +
+          "!"
+      );
+      inteiro = "a";
+    } else if (isNaN(inteiro)) {
+      inteiro = "a";
+      console.log(
+        "Informe um número inteiro, positivo válido menor ou igual a " +
+          limite +
+          "!"
+      );
+    }
+  } while (inteiro == "a");
+  return inteiro;
+}
+
+function criarMatriz(linhas, colunas) {
+  const matriz = [];
+  for (let i = 0; i < linhas; i++) {
+    matriz[i] = [];
+    for (let j = 0; j < colunas; j++) {
+      matriz[i][j] = numeroAleatorio();
+    }
+  }
+  return matriz;
+}
+
+function multiplicarMatrizes(matrizA, matrizB) {
+  let linhasA = matrizA.length;
+  let colunasA = matrizA[0].length;
+  let linhasB = matrizB.length;
+  let colunasB = matrizB[0].length;
+
+  if (colunasA !== linhasB) {
+    console.error(
+      "As dimensões das matrizes não são compatíveis para a multiplicação."
+    );
+    return;
+  }
+
+  let resultado = [];
+  for (let i = 0; i < linhasA; i++) {
+    resultado[i] = new Array(colunasB).fill(0);
+  }
+
+  for (let i = 0; i < linhasA; i++) {
+    for (let j = 0; j < colunasB; j++) {
+      for (let k = 0; k < colunasA; k++) {
+        resultado[i][j] += matrizA[i][k] * matrizB[k][j];
+      }
+    }
+  }
+
+  return resultado;
+}
+
+function numeroAleatorio() {
+  return Math.floor(Math.random() * 21) - 10; // Gera números entre -10 e 10
+}
+
+const LINHAS = 3;
+const COLUNAS = 5; // Modificado para 5 colunas
+
+const MATRIZ = criarMatriz(LINHAS, COLUNAS);
+const MATRIZ2 = criarMatriz(LINHAS, COLUNAS);
+
+const RESULTADOMULTIPLICACAOMATRIZ = multiplicarMatrizes(MATRIZ, MATRIZ2);
+
+console.log("Matriz A:");
+imprimirMatriz(MATRIZ);
+console.log("\nMatriz B:");
+imprimirMatriz(MATRIZ2);
+console.log("\nProduto das matrizes A e B:");
+imprimirMatriz(RESULTADOMULTIPLICACAOMATRIZ);
